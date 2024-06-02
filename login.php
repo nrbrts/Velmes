0e6b7a0837e50a5824c5c3a81c0ddaced61c45c8
nrbrts <148334939+nrbrts@users.noreply.github.com>
2024-06-02 15:15:04 +0300

    neeksiste

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $passwordError = "Nepareiza parole!";
             }
         } else {
-            $usernameError = "Profils nēksistē!";
+            $usernameError = "Profils neeksistē!";
         }
     }
 }
